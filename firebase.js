101acf85306e91e72c5fd7174113e25a1c86d4a3
Vishwas Zadte <vishwaszadte@gmail.com>
2021-11-10 22:05:10 +0530

    added firebaseConfig in the firebase.js file

--- firebase.js
+++ firebase.js
@@ -12,5 +12,3 @@ const firebaseConfig = {
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-
-  const db = getFirestore();
--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js"; 
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDRjzK3bLjxjMoETPbegZ-6nk8iRdsTVcQ",
-    authDomain: "clone-67ce5.firebaseapp.com",
-    projectId: "clone-67ce5",
-    storageBucket: "clone-67ce5.appspot.com",
-    messagingSenderId: "779242240417",
-    appId: "1:779242240417:web:97db1025189f381badb3e0"
-  };
-  
-  // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+import { getFirestore, doc, getDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
 const db = getFirestore();
 
@@ -30,7 +16,13 @@ async function getItems() {
             price: doc.data().price
         })
     });
-    console.log(items);
+    generateItems(items);
+}
+
+function generateItems(items) {
+    items.forEach((item) => {
+
+    })
 }
 
 getItems();
